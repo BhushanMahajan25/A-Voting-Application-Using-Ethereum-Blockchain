a6a9880ab6c54a4bbd1ccd555929d6a122a696b6
Bhushan Mahajan <bhushanmahajan707@gmail.com>
2018-11-25 01:57:21 -0800

    2.Candidate List and UI

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,25 @@
 pragma solidity ^0.4.2;
 
 contract Election {
-	//store candidate
-	//read candidate
-	string public candidate; //state variable
-	//constructor
-	function Election() public{
-		candidate = "Candidate 1";
+	//model a candidate
+	struct Candidate{
+		uint id;
+		string name;
+		uint voteCount;
+	}
+	//Store Candidates
+	//Fetch Candidates
+	mapping(uint => Candidate) public candidates;
+	//store candidates count
+	uint public candidatesCount; //counter cache
+
+	function Election() public {
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+	//_name -> local var not a state var. private -> only contract does the mapping of no of candidates.
+	function addCandidate (string _name) private{
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 }
