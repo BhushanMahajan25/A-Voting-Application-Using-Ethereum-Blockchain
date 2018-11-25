0098ec6f9a28405e904afcf08021b7091d6b0714
Bhushan Mahajan <bhushanmahajan707@gmail.com>
2018-11-25 03:32:57 -0800

    3.Vote casting

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -7,6 +7,9 @@ contract Election {
 		string name;
 		uint voteCount;
 	}
+	
+	//store accounts that have voted
+	mapping(address => bool) public voters;
 	//Store Candidates
 	//Fetch Candidates
 	mapping(uint => Candidate) public candidates;
@@ -22,4 +25,22 @@ contract Election {
 		candidatesCount ++;
 		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
+
+	function vote(uint _candidateID) public {
+		//require that they haven't voted before
+		require(!voters[msg.sender]);
+
+		//require a valid candidate
+		require(_candidateID > 0 && _candidateID <= candidatesCount);
+
+		//gas used to check above both the 'requires' will not get refunded to sender
+		//hence contracts should be written bugfree as there may be wastage if not
+
+		//record that voter has voted
+		voters[msg.sender] = true; //if voter has voted set bool=true; default bool=> false
+
+		//update candidate vote count
+		candidates[_candidateID].voteCount++;
+	}
+
 }
